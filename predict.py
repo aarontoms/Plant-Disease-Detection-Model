bd789ee1b33973ddaabe947ea138bfac94777930
aarontoms <aarontoms29@gmail.com>
2025-02-15 15:33:26 +0530

    dimensions

--- predict.py
+++ predict.py
@@ -2,22 +2,22 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
 
-model = tf.keras.models.load_model("model/tomato_model.h5")
+model = tf.keras.models.load_model("model/cassava_inception.h5")
 
 
 def preprocess_image(img_path):
-    img = image.load_img(img_path, target_size=(224, 224))
+    img = image.load_img(img_path, target_size=(299, 299))
     img_array = image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
 
 
-img_path = "pics/bacterial-leaf-spot-on-tomato-Rutgers.jpg"
+img_path = "pics/gomen.jpeg"
 img_array = preprocess_image(img_path)
 predictions = model.predict(img_array)
 predicted_class = np.argmax(predictions, axis=1)
 confidence = np.max(predictions)
-if confidence < 0.7:  # Adjust as needed
+if confidence < 0.7:
     print(f"Unknown image, not in dataset with confidence {confidence}")
 else:
     print(f"Predicted class: {predicted_class[0]} with confidence {confidence}")
